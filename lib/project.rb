6413c5f954c399160a1ca75c60f254f028f94132
Eliot Swank <eliotswank@gmail.com>
2018-04-17 09:29:07 -0600

    adds project file

--- lib/project.rb
+++ lib/project.rb
@@ -2,7 +2,7 @@ class Project
   attr_reader :project_id, :name, :start_date, :end_date
 
   def initialize(data)
-    @project_id = data[:project_id]
+    @project_id = data[:project_id].to_i
     @name = data[:name]
     @start_date = data[:start_date]
     @end_date = data[:end_date]
--- test/project_test.rb
+++ test/project_test.rb
@@ -7,8 +7,20 @@ class ProjectTest < Minitest::Test
   def test_instantiation
     data = {:project_id => '123', :name => 'Widget Maker',
             :start_date => Date.new(2018, 1, 1), :end_date => Date.new(2018, 3, 4)}
-    project = Project.new()
+    project = Project.new(data)
 
     assert_instance_of Project, project
+  end
+
+  def test_attributes
+    data = {:project_id => '123', :name => 'Widget Maker',
+            :start_date => Date.new(2018, 1, 1), :end_date => Date.new(2018, 3, 4)}
+    project = Project.new(data)
+
+    assert_equal 123, project.project_id
+    assert_instance_of Integer, project.project_id
+    assert_equal 'Widget Maker', project.name
+    assert_instance_of Date, project.start_date
+    assert_instance_of Date, project.end_date
   end
 end
