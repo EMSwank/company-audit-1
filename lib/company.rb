82b99fa6a5869139946176d6d6333d50ccbe1cce
Eliot Swank <eliotswank@gmail.com>
2018-04-17 11:21:15 -0600

    continue work on iter 3

--- lib/company.rb
+++ lib/company.rb
@@ -25,17 +25,32 @@ class Company
       end
   end
 
+  def add_employees
+    @employees.each do |employee|
+      Employee.new(:employee_id => employee[0],
+                   :name => employee[1],
+                   :role => employee[2],
+                   :start_date => employee[3],
+                   :end_date => employee[4])
+      end 
+    require 'pry'; binding.pry
+
+  end
+
   def load_timesheets(filename)
     lines = CSV.readlines(filename)
     lines.each do |line|
       raise BadDataError if line.count != 4 || line.include?(nil)
       if line.count != 4
-
         return {:success => false, :error => 'bad data'}
-      end
+      else
       timesheets << line
       return {:success => true, :error => nil}
     end
+    end
   end
 
+  def find_employee_by_id(employee_id)
+
+  end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -31,18 +31,16 @@ class CompanyTest < Minitest::Test
       company.load_employees(file_2)
     end
     assert_equal 2, company.employees.count
-
+    assert_instance_of Employee, company.add_employees
   end
 
   def test_it_loads_timesheets
     company= Company.new
     file_1 = './data/timesheets.csv'
     file_2 = './data/bad_timesheets.csv'
+
     assert_equal 2, company.load_timesheets(file_1).count
     assert_equal ({:success => true, :error => nil}), company.load_timesheets(file_1)
-    assert_raises BadDataError do
-      company.load_timesheets(file_2)
-    end
     assert_equal 2, company.timesheets.count
   end
 end
